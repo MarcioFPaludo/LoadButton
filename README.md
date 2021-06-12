ecddfa2f0ff4b39e830a9270f30bf95f29080351
Vinícius Dornelles Brandão <vinicius.brandao@mobile2you.com.br>
2021-06-12 20:27:21 -0300

    Load Button Initial configuration

---
+++ README.md
@@ -0,0 +1,5 @@
+# LoadButton
+
+A description of this package
+
+
---
+++ Sources/LoadButton/LoadButtonProtocol.swift
@@ -0,0 +1,38 @@
+//
+//  LoadButtonDelegate.swift
+//  
+//
+//  Created by Vinícius Dornelles Brandão on 22/05/21.
+//
+
+import UIKit
+
+public protocol LoadButtonDelegate {
+    /**
+     The radius of the indicator.
+     */
+    var radius: CGFloat { get set }
+    /**
+     The primary color of the indicator.
+     */
+    var color: UIColor { get set }
+    /**
+     Current status of animation, read-only.
+     */
+    var isAnimating: Bool { get }
+    /**
+     Start animating.
+     */
+    func startAnimating()
+    /**
+     Stop animating and remove layer.
+     */
+    func stopAnimating()
+    /**
+     Set up the animation of the indicator.
+     
+     - Parameter layer: The layer to present animation.
+     - Parameter size:  The size of the animation.
+     */
+    func setupAnimation(in layer: CALayer, size: CGSize)
+}
